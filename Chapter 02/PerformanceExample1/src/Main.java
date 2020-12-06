002d40a846f08ca66de929cd8256d0e86be203a1
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2020-12-06 15:24:45 -0300

    end of chapter 2

--- Chapter 02/PerformanceExample1/src/Main.java
+++ Chapter 02/PerformanceExample1/src/Main.java
@@ -2,7 +2,9 @@ import java.util.Date;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(20000);
+		System.out.println("Application is running!");
 		PrimeNumbers primeNumbers = new PrimeNumbers();
 		Integer max = Integer.parseInt(args[0]);
 		primeNumbers.generateNumbers(max);
